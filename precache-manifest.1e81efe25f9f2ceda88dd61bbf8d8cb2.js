8e435c8187ff8aeb53c7662f30c24d490c479f4a
Michael Bowman <micbowman25@gmail.com>
2020-05-08 21:35:58 -0500

    Updates

---
+++ precache-manifest.1e81efe25f9f2ceda88dd61bbf8d8cb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb9a885ec517ba9d7eb6ce2e6ccb12ec",
+    "url": "/User-Directory/index.html"
+  },
+  {
+    "revision": "aa812cd8bb9e2aaa32ba",
+    "url": "/User-Directory/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "5fdd0c0480e6b0d95a7e",
+    "url": "/User-Directory/static/js/2.bffd82dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/User-Directory/static/js/2.bffd82dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa812cd8bb9e2aaa32ba",
+    "url": "/User-Directory/static/js/main.09825d96.chunk.js"
+  },
+  {
+    "revision": "f32edffdb4d555b0a488",
+    "url": "/User-Directory/static/js/runtime-main.a2464fbc.js"
+  }
+]);
